de1cc588a0f4a43313fb1dde31280302c326a591
Deepankar523 <adeepankarreddy523@gmail.com>
2026-04-12 20:02:46 +0530

    fixed merge conflicts

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, redirect, session, render_template, flash, send_file
 from db import get_db_connection
 from decimal import Decimal, InvalidOperation
@@ -962,4 +961,4 @@ def admin_user_loans(uid):
 
 if __name__ == "__main__":
     ensure_admin()
-
+    app.run(debug=True)
