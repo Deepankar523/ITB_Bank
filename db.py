4f1b4504187a230009809d30af31a2b79dc3a632
Deepankar523 <adeepankarreddy523@gmail.com>
2026-04-12 20:06:40 +0530

    fixed merge conflicts

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="sql12.freesqldatabase.com",
         user="sql12822951",
-        password="sitarama010874",
+        password="NCvHVqAwpj",
         database="sql12822951",
         port=3306
     )
